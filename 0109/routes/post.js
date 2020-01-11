bcea55092472984b51fea426218c4db6d1e76683
haksunlee39 <haksunlee39@korea.ac.kr>
2020-01-11 12:29:53 +0900

    0109 과제

--- 0109/routes/post.js
+++ 0109/routes/post.js
@@ -16,10 +16,30 @@ router.post('/', (req, res) => {
   res.status(200).json({success: true});
 });
 router.get('/', (req, res) => {
+  let value = [];
   // posts.json 값 읽어서 전달
+  if (fs.pathExistsSync(postsPath)) {
+    value = fs.readJSONSync(postsPath);
+  }
   // 200 {success: true, posts: ~~}
+  res.status(200).json({success: true, posts: value})
 })
 router.delete('/:id', (req, res) => {
+  console.log(req.cookies);  //cookies 읽어보려고 함!
+  if(req.cookies.auth === 'admin'){  //만약 auth 쿠기가 admin 이라면 들어오기
+    let posts = [];
+    if (fs.pathExistsSync(postsPath)) {
+      posts = fs.readJSONSync(postsPath); //json 파일 읽어오기
+    }
+    let index = posts.findIndex((value)=> value.id === req.params.id); //그 id 를 가진 값의 index 가져오기
+    if(index >= 0) { //못찾았느면 index는 -1 즉 찾았으면 지우기!
+      posts.splice(index,1);
+      fs.writeJsonSync(postsPath, [...posts]);
+    }
+    res.status(200).json({success: true});
+  } else {
+    res.status(403).json({success: false});
+  }
   // cookies에 auth 키에 admin이라는 value가 있을 경우
       // posts.json에서 id가 일치하는 부분 삭제하고 저장 & 200 {success: true} 보내기
   // 없을 경우
