980842149516fd0637b6c48a3a07ccaccc6f4452
haksunlee39 <haksunlee39@korea.ac.kr>
2020-01-11 12:53:32 +0900

    0110 과제

--- 0109/routes/index.js
+++ 0109/routes/index.js
@@ -11,4 +11,45 @@ router.use('/err', (req, res, next) => {
   next('error occurs!');
 });
 
+//await가 적용되는 함수!
+const foo1 = (num, sec) => {
+  return new Promise((resolve, reject) => {
+    setTimeout( ()=> {
+      console.log(num);
+      resolve('Promise!!');
+    }, sec);
+  })
+}
+
+//promise를 return 하지 않아서 await 가 적용되지 않는 함수!
+const foo2 = (num, sec) => {
+  setTimeout( ()=> {
+    console.log(num);
+  }, sec);
+}
+
+async function test1(){
+  await foo1('a', 5000);
+  await foo1('b', 3000);
+  await foo1('c', 1000);
+};
+
+async function test2(){
+  await foo2('a', 5000);
+  await foo2('b', 3000);
+  await foo2('c', 1000);
+};
+
+router.get('/async', async function(req, res, next) {
+  try {
+    test1();
+  } catch (err) {
+    next(err);
+  }
+});
+router.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({msg: 'error occur!'});
+});
+
 module.exports = router;
